5c5e311751f2564a6e9d3a4e3c4263479f19532f
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2019-03-26 14:53:16 +0100

    fix: don't use app_redirect base url on login success

--- src/access.lua
+++ src/access.lua
@@ -177,7 +177,8 @@ function _M.run(conf)
 	  callback_url = ngx.var.scheme .. "://" .. ngx.var.host .. path_prefix .. "/oauth2/callback"
 	elseif pl_stringx.endswith(path_prefix, "/oauth2/callback") then --We are in the callback of our proxy
 	  callback_url = ngx.var.scheme .. "://" .. ngx.var.host .. path_prefix
-	  handle_callback(conf, callback_url)
+      -- handle_callback(conf, callback_url)
+      handle_callback(conf, path_prefix:split('/oauth2/callback')[0])
 	else
 	  callback_url = ngx.var.scheme .. "://" .. ngx.var.host .. path_prefix .. "/oauth2/callback"
 	end
